8fc9d8c643019e1ef924a64751ab0652a83001e6
Vince Kuperavage <vince.kuperavage@gmail.com>
2021-03-08 17:35:01 -0600

    Update app.js

--- client/app.js
+++ client/app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Navbar, Video } from './components'
+import { Navbar } from './components'
 import Parallax from './components/parallax.js'
 import Icons from './components/icons.js'
 
@@ -13,7 +13,6 @@ const App = () => {
             <Parallax />
             <Icons />
             <Routes />
-            <Video />
         </div>
     )
 }
