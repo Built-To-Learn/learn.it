254fad4bb1a934dd3da3da55d182d5eb01b6515d
Jacob Goldstein <by.jakeg@gmail.com>
2021-03-11 21:51:57 -0500

    working signup yay

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -5,36 +5,85 @@ import { authenticate } from '../store'
 const AuthForm = (props) => {
     const { name, displayName, handleSubmit, error } = props
 
-    return (
-        <div className="section container center-align" style={{width: "35%"}}>
-            <h1 className="center-align">{name}</h1>
-            <div className="z-depth-1 grey lighten-3 row"
-                style={{display: "inline-block", padding: "32px 48px 0px 48px"}} >
+    if(name === "login"){
+        return (
+            <div className="section container center-align" style={{width: "35%"}}>
+                <h1 className="center-align">{name}</h1>
+                <div className="z-depth-1 grey lighten-3 row"
+                    style={{display: "inline-block", padding: "32px 48px 0px 48px"}} >
 
-                <form className="row" onSubmit={handleSubmit} name={name}>
-                    <div className="col s12 input-field">
-                        <label htmlFor="email">
-                            <small>Email</small>
-                        </label>
-                        <input name="email" type="text" />
-                    </div>
-                    <div className="col s12 input-field validate">
-                        <label htmlFor="password">
-                            <small>Password</small>
-                        </label>
-                        <input name="password" type="password" />
-                    </div>
-                    <div>
-                        <button className="btn" type="submit">{displayName}</button>
-                    </div>
-                    {error && error.response && <div> {error.response.data} </div>}
-                </form>
+                    <form className="row" onSubmit={handleSubmit} name={name}>
+                        <div className="col s12 input-field">
+                            <label htmlFor="email">
+                                <small>Email</small>
+                            </label>
+                            <input name="email" type="text" />
+                        </div>
+                        <div className="col s12 input-field validate">
+                            <label htmlFor="password">
+                                <small>Password</small>
+                            </label>
+                            <input name="password" type="password" />
+                        </div>
+                        <div>
+                            <button className="btn" type="submit">{displayName}</button>
+                        </div>
+                        {error && error.response && <div> {error.response.data} </div>}
+                    </form>
+                </div>
+                {window.githubURL && (
+                    <a href={window.githubURL}>Login / Register Via Github </a>
+                )}
             </div>
-            {window.githubURL && (
-                <a href={window.githubURL}>Login / Register Via Github </a>
-            )}
-        </div>
-    )
+        )
+    }
+
+    if(name === "signup"){
+        return (
+            <div className="section container center-align" style={{width: "35%"}}>
+                <h1 className="center-align">{name}</h1>
+                <div className="z-depth-1 grey lighten-3 row"
+                    style={{display: "inline-block", padding: "32px 48px 0px 48px"}} >
+
+                    <form className="row" onSubmit={handleSubmit} name={name}>
+                    <div className="col s12 input-field">
+                            <label htmlFor="fullname">
+                                <small>Full Name</small>
+                            </label>
+                            <input name="fullname" type="text" />
+                        </div>
+                        <div className="col s12 input-field">
+                            <label htmlFor="username">
+                                <small>Username</small>
+                            </label>
+                            <input name="username" type="text" />
+                        </div>
+                        <div className="col s12 input-field">
+                            <label htmlFor="email">
+                                <small>Email</small>
+                            </label>
+                            <input name="email" type="text" />
+                        </div>
+                        <div className="col s12 input-field">
+                            <label htmlFor="password">
+                                <small>Password</small>
+                            </label>
+                            <input name="password" type="password" />
+                        </div>
+                        <div>
+                            <button className="btn" type="submit">{displayName}</button>
+                        </div>
+                        {error && error.response && <div> {error.response.data} </div>}
+                    </form>
+                </div>
+                {window.githubURL && (
+                    <a href={window.githubURL}>Login / Register Via Github </a>
+                )}
+            </div>
+        )
+    }
+
+
 }
 
 /**
@@ -67,11 +116,17 @@ const mapDispatch = (dispatch) => {
             const formName = evt.target.name
             const email = evt.target.email.value
             const password = evt.target.password.value
-            if(email && password){
-                console.log(email, password)
-                dispatch(authenticate(email, password, formName))
-            }else{
-                console.log("NO INPUTS")
+
+            if(formName === "signup"){
+                const username = evt.target.username.value
+                const fullName = evt.target.fullname.value
+                dispatch(authenticate(formName, email, password, fullName, username ))
+            }
+
+            if(formName === "login"){
+                if(email && password){
+                    dispatch(authenticate(formName, email, password ))
+                }
             }
 
 
