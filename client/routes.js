354d45dde557660fe51a40c1f6e673e77b886a93
kuperavv <vince.kuperavage@gmail.com>
2021-03-12 17:16:27 -0600

    piped components in

--- client/routes.js
+++ client/routes.js
@@ -1,33 +1,38 @@
-
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
-import { Login, Signup, Home, VideoChat, Dashboard, Parallax, Landing } from './components';
+import {
+  Login,
+  Signup,
+  Home,
+  VideoChat,
+  Dashboard,
+  Parallax,
+  Landing,
+} from './components';
 import { CreateNewCourse } from './components/createclass.js';
 import { me } from './store';
-import CoursesView from './components/courses-view.js'
-
+import CoursesView from './components/courses-view.js';
 
 /**
  * COMPONENT
  */
 class Routes extends Component {
-    componentDidMount() {
-        this.props.loadInitialData()
-    }
+  componentDidMount() {
+    this.props.loadInitialData();
+  }
 
-    render() {
-        const { isLoggedIn } = this.props
-
+  render() {
+    const { isLoggedIn } = this.props;
 
     return (
       <div>
         {isLoggedIn ? (
           <Switch>
-            <Route exact path="/home" component={Home} />
+            <Route exact path="/home" component={Landing} />
             <Route exact path="/createcourse" component={CreateNewCourse} />
-            <Route exact path="/viewcourses"component={CoursesView}/>
-            <Route path="/dashboard" component={Dashboard} />
+            <Route exact path="/viewcourses" component={CoursesView} />
+            {/* <Route path="/dashboard" component={Dashboard} /> */}
             <Redirect to="/home" />
           </Switch>
         ) : (
@@ -35,36 +40,35 @@ class Routes extends Component {
             <Route path="/" exact component={Parallax} />
             <Route path="/login" exact component={Login} />
             <Route path="/signup" exact component={Signup} />
-            <Route path="/videochat" component={VideoChat} />
-            <Route path="/landing" component={Landing} />
+            {/* <Route path="/videochat" component={VideoChat} /> */}
+            {/* <Route path="/landing" component={Landing} /> */}
             <Route exact path="/createcourse" component={CreateNewCourse} />
           </Switch>
         )}
       </div>
     );
   }
-
 }
 
 /**
  * CONTAINER
  */
 const mapState = (state) => {
-    return {
-        // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
-        // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
-        isLoggedIn: !!state.auth.id,
-    }
-}
+  return {
+    // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
+    // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
+    isLoggedIn: !!state.auth.id,
+  };
+};
 
 const mapDispatch = (dispatch) => {
-    return {
-        loadInitialData() {
-            dispatch(me())
-        },
-    }
-}
+  return {
+    loadInitialData() {
+      dispatch(me());
+    },
+  };
+};
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
-export default withRouter(connect(mapState, mapDispatch)(Routes))
+export default withRouter(connect(mapState, mapDispatch)(Routes));
