ee8571b5ff05dcfd5e5d2b7c589f3509c379a221
kuperavv <vince.kuperavage@gmail.com>
2021-03-17 22:28:17 -0500

    course and enrollment fixes

--- client/components/courses-view.js
+++ client/components/courses-view.js
@@ -1,54 +1,50 @@
-import React, { useState, useEffect } from 'react'
-import { connect } from 'react-redux'
-import { logout } from '../store'
-import { loadCourses } from '../store/courses'
-import M from 'materialize-css'
-import CourseCard from './course-card'
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { logout } from '../store';
+import { loadCourses } from '../store/courses';
+import M from 'materialize-css';
+import CourseCard from './course-card';
 
 class CoursesView extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {}
-    }
-    async componentDidMount() {
-        this.props.getCourses()
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  async componentDidMount() {
+    this.props.getCourses();
+  }
 
-    render() {
-        if (this.props.courses.length !== 0) {
-            const courses = this.props.courses
+  render() {
+    if (this.props.courses.length !== 0) {
+      const courses = this.props.courses.all;
 
-            return courses.map((course) => {
-                return (
-                    <CourseCard
-                        className="course_list"
-                        course={course}
-                        key={course.id}
-                    />
-                )
-            })
-        } else {
-            return <div>No Courses</div>
-        }
+      return courses.map((course) => {
+        return (
+          <CourseCard className="course_list" course={course} key={course.id} />
+        );
+      });
+    } else {
+      return <div>No Courses</div>;
     }
+  }
 }
 
 /**
  * CONTAINER
  */
 const mapState = (state) => {
-    return {
-        isLoggedIn: !!state.auth.id,
-        courses: state.courses,
-    }
-}
+  return {
+    isLoggedIn: !!state.auth.id,
+    courses: state.courses,
+  };
+};
 
 const mapDispatch = (dispatch) => {
-    return {
-        getCourses: () => {
-            dispatch(loadCourses())
-        },
-    }
-}
+  return {
+    getCourses: () => {
+      dispatch(loadCourses());
+    },
+  };
+};
 
-export default connect(mapState, mapDispatch)(CoursesView)
+export default connect(mapState, mapDispatch)(CoursesView);
--- client/store/enrollments.js
+++ client/store/enrollments.js
@@ -1,49 +1,51 @@
-import axios from 'axios'
-const ENROLL_IN_COURSE = 'ENROLL_IN_COURSE'
-const LOAD_ENROLLMENTS = 'LOAD_ENROLLMENTS'
+import axios from 'axios';
+const ENROLL_IN_COURSE = 'ENROLL_IN_COURSE';
+const LOAD_ENROLLMENTS = 'LOAD_ENROLLMENTS';
 
-export const _enrollInCourse = (enrollment) => ({
-    type: ENROLL_IN_COURSE,
-    enrollment,
-})
+export const _enrollInCourse = (enrollments) => ({
+  type: ENROLL_IN_COURSE,
+  enrollments,
+});
 
 export const _loadEnrollments = (enrollments) => ({
-    type: LOAD_ENROLLMENTS,
-    enrollments,
-})
+  type: LOAD_ENROLLMENTS,
+  enrollments,
+});
 
 export const enrollInCourse = (courseId, userId, title) => {
-    try {
-        return async (dispatch) => {
-            await axios.post(`/api/enrollments`, {
-                courseId,
-                userId,
-            })
+  try {
+    return async (dispatch) => {
+      const enrollments = (
+        await axios.post(`/api/enrollments`, {
+          courseId,
+          userId,
+        })
+      ).data;
 
-            dispatch(_enrollInCourse({ courseId: courseId, title: title }))
-        }
-    } catch (err) {
-        console.log(err)
-    }
-}
+      dispatch(_enrollInCourse(enrollments));
+    };
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export const loadEnrollments = (userId) => {
-    return async (dispatch) => {
-        const enrollments = (await axios.get(`/api/enrollments/${userId}`)).data
-        dispatch(_loadEnrollments(enrollments))
-    }
-}
+  return async (dispatch) => {
+    const enrollments = (await axios.get(`/api/enrollments/${userId}`)).data;
+    dispatch(_loadEnrollments(enrollments));
+  };
+};
 
-const initialState = []
+const initialState = [];
 
 export default function (state = initialState, action) {
-    switch (action.type) {
-        case LOAD_ENROLLMENTS:
-            return action.enrollments
-        case ENROLL_IN_COURSE:
-            return [...state, { course: action.enrollment }]
-        // why is this undefeind
-        default:
-            return state
-    }
+  switch (action.type) {
+    case LOAD_ENROLLMENTS:
+      return action.enrollments;
+    case ENROLL_IN_COURSE:
+      return action.enrollments;
+    // why is this undefeind
+    default:
+      return state;
+  }
 }
